c3e704a260dd5583dc2d03a550a3655cd0906ea4
jiafeng <283678339@qq.com>
2017-12-23 12:56:35 +0000

    Finsh basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由<a href="#">jiafeng编码和实现</a> <span style="color:#e27575;font-size: 14px;">❤</span>
+        </p>
+        <p class="pull-right"><a href="tencent://Message/?Uin=283678339&websiteName=www.oicqzone.com&Menu=yes">QQ联系我</a>
+            
+        </p>
+    </div>
+</footer>
