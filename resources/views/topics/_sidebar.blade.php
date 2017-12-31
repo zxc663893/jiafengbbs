055e7e0d0373e048009de07b9cd7c93815e688f8
jiafeng <283678339@qq.com>
2017-12-31 08:33:43 +0000

    User can create topic

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,3 +1,7 @@
-<div>
-    右边导航栏
+<div class="panel panle-default">
+    <div class="panel-body">
+        <a href="{{ route('topics.create') }}" class="btn btn-success btn-block" aria-label="Left Alian">
+            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>新建帖子
+        </a>
+    </div>
 </div>
