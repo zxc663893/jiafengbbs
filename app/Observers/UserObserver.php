d3ad165120be7a2971b74fd5954772cf999fbb82
jiafeng <283678339@qq.com>
2018-01-06 05:17:21 +0000

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,11 @@ class UserObserver
     {
         //
     }
+    public function saving()
+    {
+      //这样写拓展性更高，只有空的时候才指定默认头像
+      if (empty($user->avatar)){
+        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+      }
+    }
 }
